d3bea9dcbcde9851ee08077b9fc00cfb644682bd
hoangtrung <hoangtrunga1k55@gmail.com>
2021-07-31 15:51:08 +0700

    install lfm & add in18

---
+++ resources/lang/en/partner.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'list_hotel' => 'List Hotel',
+    'name' => 'Name',
+    'description' => 'Description',
+    'province' => 'Province',
+    'actions' => 'Actions',
+    'status' => 'Status',
+    'booking' => 'Booking',
+    'hotel' => 'Hotel',
+    'add_hotel' => 'Add hotel',
+    'register_hotel' => 'Register Hotel',
+    'name_hotel' => 'Name hotel',
+    'name_placeholder' => 'Please enter name of your hotel',
+    'description_placeholder' => 'Please enter description of your hotel',
+    'province_select' => 'Select your province',
+    'image' => 'Image',
+    'create' => 'Create',
+];
