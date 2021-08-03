8095d77f9133a212ffef704010fa9f3748aa4985
hoangtrung <hoangtrunga1k55@gmail.com>
2021-08-03 17:22:35 +0700

    localization for auth

--- resources/views/cms/login.blade.php
+++ resources/views/cms/login.blade.php
@@ -15,71 +15,74 @@
 
 </head>
 
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <b>@lang('login.cms_booking')</b>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">@lang('login.start_session')</p>
-            <form action="{{ route('auth.login') }}" method="post">
-                @csrf
-                @error('email')
-                <small class="text-danger rule">{{ $message }}</small>
-                @enderror
-                <div class="input-group mb-3">
-                    <input type="email" name="email" class="form-control" value="{{ old('email',$request->email ?? null) }}" placeholder="@lang('login.email')">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                </div>
-                @error('password')
-                <small class="text-danger rule">{{ $message }}</small>
-                @enderror
-                <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control" value="{{ old('password',$request->password ?? null) }}" placeholder="@lang('login.password')">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember">
-                                @lang('login.remember_me')
-                            </label>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">@lang('login.sign_in')</button>
-                    </div>
-                    <!-- /.col -->
-                </div>
-            </form>
-
-            <div class="social-auth-links text-center mb-3">
-                <p>- @lang('login.or') -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i> @lang('login.facebook')
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i> @lang('login.google')
-                </a>
-            </div>
-            <!-- /.social-auth-links -->
-            <p class="mb-0">
-                <a href="{{ route('auth.register') }}" class="text-center"> @lang('login.register')</a>
-            </p>
+<body>
+    @include('cms.layouts.lang')
+<div class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <b>@lang('login.cms_booking')</b>
         </div>
-        <!-- /.login-card-body -->
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">@lang('login.start_session')</p>
+                <form action="{{ route('auth.login') }}" method="post">
+                    @csrf
+                    @error('email')
+                        <small class="text-danger rule">{{ $message }}</small>
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control" value="{{ old('email',$request->email ?? null) }}" placeholder="@lang('login.email')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    @error('password')
+                        <small class="text-danger rule">{{ $message }}</small>
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control" value="{{ old('password',$request->password ?? null) }}" placeholder="@lang('login.password')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">
+                                    @lang('login.remember_me')
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary btn-block">@lang('login.sign_in')</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <div class="social-auth-links text-center mb-3">
+                    <p>- @lang('login.or') -</p>
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i> @lang('login.facebook')
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i> @lang('login.google')
+                    </a>
+                </div>
+                <!-- /.social-auth-links -->
+                <p class="mb-0">
+                    <a href="{{ route('auth.register') }}" class="text-center"> @lang('login.register')</a>
+                </p>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
     </div>
 </div>
 <!-- /.login-box -->
--- resources/views/cms/register.blade.php
+++ resources/views/cms/register.blade.php
@@ -11,83 +11,86 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('bower_components/adminlte3/assets_adminlte3/dist/css/adminlte.min.css') }}">
 </head>
-<body class="hold-transition register-page">
-<div class="register-box">
-    <div class="register-logo">
-        <b>@lang('register.cms_booking')</b>
-    </div>
-    <div class="card">
-        <div class="card-body register-card-body">
-            <p class="login-box-msg">@lang('register.partner')</p>
-            <form action="{{ route('auth.register') }}" method="post">
-                @csrf
-                @error('name')
-                <small class="text-danger rule">{{ $message }}</small>
-                @enderror
-                <div class="input-group mb-3">
-                    <input type="text" name="name" class="form-control" value="{{ old('name',$request->name ?? null) }}" placeholder="@lang('register.name')">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-user"></span>
-                        </div>
-                    </div>
-                </div>
-                @error('email')
-                <small class="text-danger rule">{{ $message }}</small>
-                @enderror
-                <div class="input-group mb-3">
-                    <input type="email" name="email" class="form-control" value="{{ old('email',$request->email ?? null) }}" placeholder="@lang('register.email')">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-                </div>
-                @error('password')
-                <small class="text-danger rule">{{ $message }}</small>
-                @enderror
-                <div class="input-group mb-3">
-                    <input type="password" name="password" class="form-control" value="{{ old('password',$request->password ?? null) }}" placeholder="@lang('register.password')">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
-                @error('password')
-                <small class="text-danger rule">{{ $message }}</small>
-                @enderror
-                <div class="input-group mb-3">
-                    <input type="password" name="password_confirmation" class="form-control" value="{{ old('password',$request->password ?? null) }}" placeholder="@lang('register.confirm')">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="container">
-                        <button type="submit" class="btn btn-primary btn-block">@lang('register.register')</button>
-                    </div>
-                </div>
-            </form>
-
-            <div class="social-auth-links text-center">
-                <p>- @lang('register.or') -</p>
-                <a href="#" class="btn btn-block btn-primary">
-                    <i class="fab fa-facebook mr-2"></i>
-                    @lang('register.facebook')
-                </a>
-                <a href="#" class="btn btn-block btn-danger">
-                    <i class="fab fa-google-plus mr-2"></i>
-                    @lang('register.google')
-                </a>
-            </div>
-
-            <a href="{{ asset('auth.registerForm') }}" class="text-center">@lang('register.have_account')</a>
+<body>
+    @include('cms.layouts.lang')
+<div class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <b>@lang('register.cms_booking')</b>
         </div>
-        <!-- /.form-box -->
-    </div><!-- /.card -->
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">@lang('register.partner')</p>
+                <form action="{{ route('auth.register') }}" method="post">
+                    @csrf
+                    @error('name')
+                        <small class="text-danger rule">{{ $message }}</small>
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input type="text" name="name" class="form-control" value="{{ old('name',$request->name ?? null) }}" placeholder="@lang('register.name')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    @error('email')
+                        <small class="text-danger rule">{{ $message }}</small>
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control" value="{{ old('email',$request->email ?? null) }}" placeholder="@lang('register.email')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    @error('password')
+                        <small class="text-danger rule">{{ $message }}</small>
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control" value="{{ old('password',$request->password ?? null) }}" placeholder="@lang('register.password')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    @error('password')
+                        <small class="text-danger rule">{{ $message }}</small>
+                    @enderror
+                    <div class="input-group mb-3">
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password',$request->password ?? null) }}" placeholder="@lang('register.confirm')">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="container">
+                            <button type="submit" class="btn btn-primary btn-block">@lang('register.register')</button>
+                        </div>
+                    </div>
+                </form>
+
+                <div class="social-auth-links text-center">
+                    <p>- @lang('register.or') -</p>
+                    <a href="#" class="btn btn-block btn-primary">
+                        <i class="fab fa-facebook mr-2"></i>
+                        @lang('register.facebook')
+                    </a>
+                    <a href="#" class="btn btn-block btn-danger">
+                        <i class="fab fa-google-plus mr-2"></i>
+                        @lang('register.google')
+                    </a>
+                </div>
+
+                <a href="{{ asset('auth.registerForm') }}" class="text-center">@lang('register.have_account')</a>
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
+    </div>
 </div>
 <!-- /.register-box -->
 
