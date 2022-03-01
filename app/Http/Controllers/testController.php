dc44ea8a8ac02bfee5e69e4a19677e386f37e3a6
hoangtrung <hoangtrunga1k55@gmail.com>
2022-03-01 14:08:05 +0700

    test  sun ci

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class testController extends Controller
+{
+    public function test(){
+         $aa = 1;
+        return $aa;
+    }
+}
